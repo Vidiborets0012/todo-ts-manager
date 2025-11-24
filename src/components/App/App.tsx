899b941914674f5fcc447be7ddca4fc46032d122
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-24 17:20:07 +0200

    define the Todo interface, define the basic state of the task list

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import type { Todo } from "../../types/todo";
+
+const initialTodos: Todo[] = [
+  { id: 1, text: "Вивчити TypeScript", completed: true },
+  { id: 2, text: "Реалізувати Todo List", completed: false },
+  { id: 3, text: "Задеплоїти проект", completed: false },
+];
+
 export default function App() {
-  return <></>;
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+
+  console.log("todos:", todos);
+
+  return (
+    <>
+      <h1>Менеджер Завдань (Todo List)</h1>
+    </>
+  );
 }
